896fda97a10ab760d064c865b953c69d1a76fccd
Emil Nazyrov <72194705+EmilNazyrov@users.noreply.github.com>
2023-04-13 02:27:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,5 +10,5 @@ def solution(x_success: int,
              x_cnt: int,
              y_success: int,
              y_cnt: int) -> bool:
-    p = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+    p = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='two-sided')
     return p[1] < alpha
