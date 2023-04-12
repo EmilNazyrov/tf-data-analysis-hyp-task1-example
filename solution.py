afccff7117d1b5107b5d962e2b666a9eb7bc3fe6
Emil Nazyrov <72194705+EmilNazyrov@users.noreply.github.com>
2023-04-13 02:09:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 from statsmodels.stats.proportion import proportions_ztest
 
-
 chat_id = 702381553
+alpha = 0.02
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
+
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
              y_cnt: int) -> bool:
     p = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
-    return p[1] >= 0.52
+    return p[1] > alpha
