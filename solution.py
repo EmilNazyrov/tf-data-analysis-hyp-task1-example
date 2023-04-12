a245681a7d13ecd0171c8cd30294bd46074c0e1e
Emil Nazyrov <72194705+EmilNazyrov@users.noreply.github.com>
2023-04-13 01:56:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,4 +10,4 @@ def solution(x_success: int,
              y_success: int, 
              y_cnt: int) -> bool:
     p = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
-    return p[1] > 0.5
+    return p[1] >= 0.52
