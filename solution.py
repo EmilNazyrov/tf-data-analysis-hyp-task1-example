f30ba14e90dc65fb60380a73bc6ea6ebc676905a
Emil Nazyrov <72194705+EmilNazyrov@users.noreply.github.com>
2023-04-13 13:13:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.proportion import proportions_ztest
+from scipy.stats import norm
 
 chat_id = 702381553
-alpha = 0.02
 
 
 def solution(x_success: int,
              x_cnt: int,
              y_success: int,
              y_cnt: int) -> bool:
-    p = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='two-sided')
-    return p[1] < alpha
+    alpha = 0.02
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    z = (p1 - p2) / (p * (1 - p) * ((1 / x_cnt) + (1 / y_cnt))) ** 0.5
+    p_val = norm.sf(z)
+    return p_val < alpha
+  
